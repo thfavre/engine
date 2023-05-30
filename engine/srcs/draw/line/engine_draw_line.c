1557549b05dadca3b1637d3064c474d39e83d834
thfavre <thomas.favre22@yahoo.fr>
2023-05-30 17:12:26 +0200

    code cleanup

--- engine/srcs/draw/line/engine_draw_line.c
+++ engine/srcs/draw/line/engine_draw_line.c
@@ -10,30 +10,30 @@ void engine_draw_pixel(t_img *img, t_vector2 pos, int color);
 void engine_draw_rect(t_img *img, t_rect rect, int color); // TODO remove
 
 void draw_line_step(t_img *img, t_vector2 *pos, t_vector2 *d, t_vector2 *s, int *err, int color, int thickness) {
-    t_rect rect = {{pos->x - thickness / 2, pos->y - thickness / 2}, {thickness, thickness}};
-    engine_draw_rect(img, rect, color);
+	t_rect rect = {{pos->x - thickness / 2, pos->y - thickness / 2}, {thickness, thickness}};
+	engine_draw_rect(img, rect, color);
 
-    int e2 = 2 * *err;
-    if (e2 > -d->y) {
-        *err -= d->y;
-        pos->x += s->x;
-    }
-    if (e2 < d->x) {
-        *err += d->x;
-        pos->y += s->y;
-    }
+	int e2 = 2 * *err;
+	if (e2 > -d->y) {
+		*err -= d->y;
+		pos->x += s->x;
+	}
+	if (e2 < d->x) {
+		*err += d->x;
+		pos->y += s->y;
+	}
 }
 
 void engine_draw_line(t_img *img, t_vector2 start_pos, t_vector2 end_pos, int color, int thickness) {
-    t_vector2 d = {abs(end_pos.x - start_pos.x), abs(end_pos.y - start_pos.y)};
-    t_vector2 s = {(start_pos.x < end_pos.x) ? 1 : -1, (start_pos.y < end_pos.y) ? 1 : -1};
-    t_vector2 pos = start_pos;
-    int err = d.x - d.y;
+	t_vector2 d = {abs(end_pos.x - start_pos.x), abs(end_pos.y - start_pos.y)};
+	t_vector2 s = {(start_pos.x < end_pos.x) ? 1 : -1, (start_pos.y < end_pos.y) ? 1 : -1};
+	t_vector2 pos = start_pos;
+	int err = d.x - d.y;
 
-    while (true) {
-        draw_line_step(img, &pos, &d, &s, &err, color, thickness);
-        if (pos.x == end_pos.x && pos.y == end_pos.y) break;
-    }
+	while (true) {
+		draw_line_step(img, &pos, &d, &s, &err, color, thickness);
+		if (pos.x == end_pos.x && pos.y == end_pos.y) break;
+	}
 }
 
 // old algorithm
